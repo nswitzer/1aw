8e38c6e9d2569fe4424e1602746c2e85867f5627
Nick Switzer <nswitzer@elevatedthird.com>
2016-03-31 20:14:24 -0600

    adds cookie to preloader to prevent display on every page load

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 (function($) {
 	$(window).load(function() {
-		$('.page-wrapper').hide();
-		$('#preloader').delay('1000').fadeOut('slow', function() {
-			$(this).remove();
-			$('.page-wrapper').fadeIn('2000');
-		});
+		var $preloader = $('#preloader');
+		
+		if (Cookies.get('preloader') == '1') {
+			$preloader.remove();
+		} else {
+			$('.page-wrapper').hide();
+			$preloader.delay('1000').fadeOut('slow', function() {
+				$(this).remove();
+				$('.page-wrapper').fadeIn('2000');
+				Cookies.set('preloader', '1');
+			});
+		}
 	});
 })(jQuery);
